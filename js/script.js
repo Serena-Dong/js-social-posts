20c835ce0088acb6210668618b9121bf033389da
Serena-Dong <serenadong3159@icloud.com>
2022-12-23 17:17:16 +0100

    stampa dei contenuti in pagina

--- js/script.js
+++ js/script.js
@@ -1,34 +1,24 @@
 /*
-# Milestone 1
-Creiamo il nostro array di oggetti che rappresentano ciascun post.
-Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
-- id del post, numero progressivo da 1 a n
-- nome autore,
-- foto autore (potrebbe mancare a qualcuno),
-- data in formato americano (mm-gg-yyyy),
-- testo del post,
-- immagine (non tutti i post devono avere una immagine),
-- numero di likes.
-Non è necessario creare date casuali, inventatele*
-Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=3)
+#Milestone 2
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 */
 
 const data = [
     
     { id: 1,
     name: 'Rachael Zane', 
-    profilePic: 'https://api.lorem.space/image/fashion?w=150&h=150', 
+    profilePic: 'https://api.lorem.space/image/fashion?w=1000&h=1000', 
     date: '12-22-2022', 
     text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.', 
-    image: 'https://api.lorem.space/image/furniture?w=150&h=150', 
+    image: 'https://api.lorem.space/image/pizza?w=1000&h=500', 
     likes: 56,
     },
     { id: 2,
     name: 'Jessica Day', 
-    profilePic: 'https://api.lorem.space/image/fashion?w=150&h=150', 
+    profilePic: 'https://api.lorem.space/image?w=1000&h=1000', 
     date: '12-20-2022', 
     text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.', 
-    image: 'https://api.lorem.space/image/furniture?w=150&h=150', 
+    image: '', 
     likes: 23,
     },
     { id: 3,
@@ -36,23 +26,67 @@ const data = [
     profilePic: '', 
     date: '12-15-2022', 
     text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.', 
-    image: '', 
+    image: 'https://api.lorem.space/image/furniture?w=1000&h=500', 
     likes: 64,
     },
     { id: 4,
     name: 'Mike Ross', 
-    profilePic: 'https://api.lorem.space/image/fashion?w=150&h=150', 
+    profilePic: 'https://api.lorem.space/image/watch?w=1000&h=1000&hash=7F5AE56A', 
     date: '12-07-2022', 
     text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.', 
-    image: 'https://api.lorem.space/image/furniture?w=150&h=150', 
+    image: 'https://api.lorem.space/image/car?w=1000&h=500', 
     likes: 87,
     },
     { id: 5,
     name: 'Winston Bishop', 
-    profilePic: 'https://api.lorem.space/image/fashion?w=150&h=150', 
+    profilePic: 'https://api.lorem.space/image/fashion?w=1000&h=1000&hash=9D9539E7', 
     date: '10-04-2022', 
     text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.', 
-    image: 'https://api.lorem.space/image/furniture?w=150&h=150', 
+    image: 'https://api.lorem.space/image/house?w=1000&h=500', 
     likes: 34,
     },
 ]
+
+//ELEMENTO PRINCIPALE
+
+const postList = document.querySelector('.posts-list');
+
+//Creo gli elementi
+let post = '';
+
+for (let i = 0; i < data.length; i++){
+    post += `
+    <div class="post">
+        <div class="post__header">
+          <div class="post-meta">
+            <div class="post-meta__icon">
+              <img class="profile-pic" src="${data[i].profilePic}" alt="Phil Mangione" />
+            </div>
+            <div class="post-meta__data">
+              <div class="post-meta__author">${data[i].name}</div>
+              <div class="post-meta__time">${data[i].date}</div>
+            </div>
+          </div>
+        </div>
+        <div class="post__text">
+        ${data[i].text}
+        </div>
+        <div class="post__image">
+          <img src="${data[i].image}" alt="" />
+        </div>
+        <div class="post__footer">
+          <div class="likes js-likes">
+            <div class="likes__cta">
+              <button class="like-button js-like-button" href="#" data-postid="1">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+              </button>
+            </div>
+            <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${data[i].likes}</b> persone</div>
+          </div>
+        </div>
+      </div>
+    `
+}
+//Inserisco in pagina
+postList.innerHTML= post;
